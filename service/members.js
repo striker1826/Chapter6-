a75744cc0621739eec59adce3d2541bb8d9b6d4f
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-10-21 14:59:50 +0900

    김민섭: members 기능 수정 - nickname  인자값 제거 22-10-21 14:59

--- service/members.js
+++ service/members.js
@@ -1,7 +1,7 @@
 const MembersRepository = require("../repository/members");
 
 const crypto = require("crypto");
-
+const key = require("나는 시크릿키다");
 function pbkdf2(password, salt, iterations, len, hashType) {
   return new Promise((resolve, reject) => {
     crypto.pbkdf2(password, salt, iterations, len, hashType, (err, key) => {
@@ -12,11 +12,10 @@ function pbkdf2(password, salt, iterations, len, hashType) {
 
 class MembersService {
   membersRepository = new MembersRepository();
-  updateMember = async (userId, nickname, password) => {
+  updateMember = async (userId, password) => {
     const hashPassword = await pbkdf2(password, key, 121381, 121, "sha512");
     const updateMember = await this.membersRepository.updateMember(
       userId,
-      nickname,
       hashPassword
     );
     return updateMember;
