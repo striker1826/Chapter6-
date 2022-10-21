8bafd692ae7b9939438a4771a9faad501a51a72a
SeoSoohyung <spartaseosu@gmail.com>
2022-10-21 15:52:16 +0900

    서수형 login,signup 추가 22-10-21 15:50

--- controller/members.js
+++ controller/members.js
@@ -1,7 +1,53 @@
 const MembersService = require("../service/members");
+const Joi = require("joi");
+
+
+const schema = Joi.object({
+  userId: Joi.String().pattern(new RegExp("^[a-zA-Z0-9]{4,20}$")).required(),
+  nickname: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  password: Joi.string().min(3).required(),
+  confirm: Joi.ref("password"),
+});
 
 class MembersController {
   membersService = new MembersService();
+
+  SignupMember = async (req, res, next) => {
+    try {
+      const { userId, nickname, password, confirm } = req.body;
+
+      await schema.validateAsync(req.body);
+      await this.membersService.createMember(
+        userId,
+        nickname,
+        password,
+        confirm
+      );
+      res.status(201).json({ message: "회원가입이 완료되었습니다." });
+    } catch (e) {
+      return res.status(400).json({ code: 400, message: e.message });
+    }
+  };
+
+  loginMember = async (req, res, next) => {
+    try {
+      const { userId, password } = req.body;
+      if (req.headers.authorization) {
+        res.status(401).send({ errorMessage: "이미 로그인이 되어있습니다." });
+        return;
+      }
+
+      const LoginUserData = await this.membersService.findByMember(
+        userId,
+        password
+      );
+      res.status(200).json({ data: LoginUserData });
+    } catch (e) {
+      res
+        .status(400)
+        .send({ message : e.message });
+    }
+  };
 
   updateMember = async (req, res, next) => {
     try {
--- service/members.js
+++ service/members.js
@@ -12,6 +12,47 @@ function pbkdf2(password, salt, iterations, len, hashType) {
 
 class MembersService {
   membersRepository = new MembersRepository();
+  createMember = async (userId, nickname, password) => {
+    try {
+      const existsUsers = await this.membersRepository.findAllMembers(nickname);
+      if (existsUsers.length !== 0) {
+        throw new Error("닉네임이 이미 존재합니다.");
+        return;
+      }
+
+      const existsUserId = await this.membersRepository.findAllMembers(userId);
+      if(existsUserId.length !== 0){
+        throw new Error("아이디가 이미 존재합니다.");
+        return
+      }
+
+      // key값?
+      const hashPassword = await pbkdf2(password, key, 121381, 121, "sha512");
+      password = hashPassword;
+      const createUserData = await this.membersRepository.createMember(
+        userId,
+        nickname,
+        password,
+      );
+      return createUserData
+    } catch (e) {
+      return 
+    }
+  };
+
+  findByMember = async (userId, password) => {
+    const hashPassword = await pbkdf2(password, key, 121381, 121, "sha512");
+    const user = await this.membersRepository.findByMember(userId,hashPassword);    
+    if (!user || hashPassword !== user.password) {
+      throw  new Error ("닉네임 또는 비밀번호가 일치하지 않습니다.");      
+    }
+    return {
+      token: jwt.sign({ id: user.id }, "seosoohyung-secret-key"),
+    };
+  };
+
+
+
   updateMember = async (userId, password) => {
     const hashPassword = await pbkdf2(password, key, 121381, 121, "sha512");
     const updateMember = await this.membersRepository.updateMember(
